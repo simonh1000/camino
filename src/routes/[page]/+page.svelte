16685182c1e32a89816e8ef6bd3ae64dc007c6e2
Simon Hampton <hotbelgo@gmail.com>
2024-05-05 08:06:22 +0200

    clear cache button

--- src/routes/[page]/+page.svelte
+++ src/routes/[page]/+page.svelte
@@ -14,6 +14,42 @@
     // let ampPrefix = "https://remix-dev.remixlabs.com/a/"
     // let ampPrefix = "http://localhost:8000/";
     let authPrefix = "https://auth.remixlabs.com/a";
+
+    const dbName = "keyval-store";
+    const objStoreName = "keyval";
+    const key = "/camino.remix";
+
+    function del() {
+        console.log("Attempting to delete");
+        // Let us open our database
+        const dbVersion = 2;
+        const request = indexedDB.open(dbName, dbVersion);
+        // var deleteRequest = indexedDB.deleteDatabase(dbName);
+        // deleteRequest.onsuccess = (evt) => {
+        //     console.log("deleted", evt);
+        // };
+        // deleteRequest.onerror = (evt) => {
+        //     console.error("delete", evt);
+        // };
+
+        request.onsuccess = (evt) => {
+            const db = request.result;
+            console.log("onsuccess", evt, db);
+        };
+        request.onupgradeneeded = (evt: any) => {
+            const db = evt.target.result;
+            console.log("onupgradeneeded", evt, db);
+            // const transaction = db.transaction([objStoreName], "readwrite");
+
+            db.deleteObjectStore(objStoreName);
+            // transaction.oncomplete = () => {
+            //     console.log("complete");
+            // };
+        };
+        request.onerror = (evt) => {
+            console.error(evt, request);
+        };
+    }
 </script>
 
 <div class="container">
@@ -28,6 +64,7 @@
             rmx-uid="svelte"
         ></rmx-remix>
     {/await}
+    <button onclick={del}>Delete</button>
 </div>
 
 <style>
