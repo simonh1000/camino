304a414b1c61c93e36c9b78fdef38fc83a41d54f
Simon Hampton <hotbelgo@gmail.com>
2025-04-26 09:46:02 +0200

    betters perms script

--- scripts/camino-permissions.js
+++ scripts/camino-permissions.js
@@ -8,8 +8,8 @@ In this case, I want to end up with
 ************************* */
 
 const AUTH = `Bearer ${process.env.TOKEN}`;
-const CLOUD_URL = "https://flymixer-broken-moon-4375.fly.dev";
-// const CLOUD_URL = "https://agt-dev.remixlabs.com";
+// const CLOUD_URL = "https://flymixer-broken-moon-4375.fly.dev";
+const CLOUD_URL = "https://agt-dev.remixlabs.com";
 const WS = "simon";
 const APP = "camino";
 
@@ -23,12 +23,10 @@ const runners = [
 const anonAgents = ["get_posts", "wave"];
 // const adminAgents = ["save", "delete"];
 
-// curl -X POST \
-//   -H "$AUTH" \
-//   -H "content-type: application/json" \
-//   -d '{"_rmx_type": "permission_grant", "subject":"all-users", "role":"runner", "resource":"workspace"}' \
-//   "$CLOUD_URL/grant-permission/$WS"
-async function grantUserRunner(email) {
+/* ************ Add runner permissions ***************** */
+
+//   -d '{"_rmx_type": "permission_grant", "subject":"user/x@gmail.com", "role":"runner", "resource":"db/camino"}' \
+async function grantRunnerToUser(email) {
     const subject = `user/${email}`;
     return fetch(`${CLOUD_URL}/grant-permission/${WS}`, {
         method: "POST",
@@ -48,15 +46,14 @@ async function grantUserRunner(email) {
 }
 
 async function createRunners() {
-    const res = await Promise.all(runners.map(grantUserRunner));
+    const res = await Promise.all(runners.map(grantRunnerToUser));
     // console.log("createRunners", res);
     return res;
 }
-// curl -X POST \
-//   -H "$AUTH" \
-//   -H "content-type: application/json" \
-//   -d '{"_rmx_type": "permission_grant", "subject":"all-users", "role":"runner", "resource":"workspace"}' \
-//   "$CLOUD_URL/grant-permission/$WS"
+
+/* ************ Anon Agents ***************** */
+
+//   -d '{"_rmx_type": "permission_grant", "subject":"anonymous", "role":"runner", "resource":"agent/camino/<agentName>"}' \
 async function grantAnonToAgent(agentName) {
     const subject = "anonymous";
     return fetch(`${CLOUD_URL}/grant-permission/${WS}`, {
@@ -76,6 +73,10 @@ async function grantAnonToAgent(agentName) {
         .then((res) => ({ ...res, name: agentName, subject }));
 }
 
+async function createAgentAnonPerms() {
+    return Promise.all(anonAgents.map(grantAnonToAgent));
+}
+/* ************ Delete Agent Permissions ***************** */
 async function removeAgentPermission(agentName, id) {
     let url = `${CLOUD_URL}/v1/ws/${WS}/app/${APP}/agent/${agentName}/permissions/${id}`;
     let res = await fetch(url, {
@@ -83,18 +84,11 @@ async function removeAgentPermission(agentName, id) {
         headers: {
             Authorization: AUTH,
         },
-    }).then((res) => res.text());
-    return res;
+    });
+    return await res.text();
 }
 
-async function removePermissions(agentName) {
-    let res = await getPermissions(agentName);
-    return Promise.all(
-        res.map((record) => removeAgentPermission(agentName, record.id))
-    );
-}
-
-async function getPermissions(agentName) {
+async function getAgentPermissions(agentName) {
     // /v1/ws/{ws}/app/{app}/permissions
     // /v1/ws/{ws}/app/{app}/agent/{agent}/permissions
     let url = `${CLOUD_URL}/v1/ws/${WS}/app/${APP}/agent/${agentName}/permissions`;
@@ -107,10 +101,34 @@ async function getPermissions(agentName) {
     return await res.json();
 }
 
+async function removeAgentAnonPerms(agentName) {
+    let res = await getAgentPermissions(agentName);
+    return Promise.all(
+        res.map((record) => removeAgentPermission(agentName, record.id))
+    );
+}
+
+/* ************ Delete DB Permissions ***************** */
+
+async function getAppPermissions(agentName) {
+    // /v1/ws/{ws}/app/{app}/permissions
+    // /v1/ws/{ws}/app/{app}/agent/{agent}/permissions
+    let url = `${CLOUD_URL}/v1/ws/${WS}/app/${APP}/permissions`;
+    let res = await fetch(url, {
+        method: "GET",
+        headers: {
+            Authorization: AUTH,
+        },
+    });
+    return await res.json();
+}
+
+/* ************Init **************************************** */
+
 async function init() {
     // Set anonymous agents
-    const anonResult = await Promise.all(anonAgents.map(grantAnonToAgent));
-    // console.log(anonResult);
+    await removeAgentAnonPerms("save");
+    const anonResult = await createAgentAnonPerms();
 
     // set users with general run permissions
     const runners = await createRunners();
@@ -118,4 +136,4 @@ async function init() {
 }
 
 init().then(console.log).catch(console.error);
-// removePermissions("save").then(console.log).catch(console.error);
+// getAppPermissions().then(console.log).catch(console.error);
